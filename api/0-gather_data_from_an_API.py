4cf919481b74d7a43e654aa11bb6cae701c2256e
Flagelo6482 <frankln6482@gmail.com>
2023-08-22 07:54:04 -0700

    Feat: Adding readme file and task :sparkles:

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Gather data from an API
+"""
+import requests
+import sys
+
+
+def main():
+    #URL de la REST API
+    BASE_URL = 'https://jsonplaceholder.typicode.com'
+
+    #ID de empleados
+    empoleye = int(sys.argv[1])
+
+    #Hacemos la solicitud GET al punto final '/todos' para obtener la lista TODO para el empleado dado
+    response = requests.get(f'{BASE_URL}/todos?userId={empoleye}')
+
+    #Analizamos la respuesta JSON y contamos la cantidad de tareas completadas
+    todos = response.json()
+    tasks_completed = [todo for todo in todos if todo['completed']]
+    num_tasks_completed = len(tasks_completed)
+    num_tasks = len(todos)
+
+    #Obtenemos el nombre del empleado del punto final '/users'
+    user_response = requests.get(f'{BASE_URL}/users/{empoleye}')
+    user_data = user_response.json()
+    empoleye_name = user_data['name']
+
+    #Imprimimos la información de la lista
+    print(f"Employee {empoleye_name} is donde with tasks ({num_tasks_completed}/{num_tasks}):")
+
+    for todo in tasks_completed:
+        print(f'\t {todo["title"]}')
+
+
+if __name__ == '__main__':
+    main()
